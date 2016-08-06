98ce32ddb5ff8cca7f28c980532490f7b6a1de12
Pier-Luc Picard <pierluc_picard@yahoo.ca>
2016-08-06 18:37:19 -0400

    Refactor

--- tests/ratio-temperature.js
+++ tests/ratio-temperature.js
@@ -6,26 +6,28 @@ var ratio = new Ratio();
 describe('Ratio Temperature',function(){
 
   it('should converte celcius to kelvin',function(){
-    return ratio.converteTemperature({value:0,unit:"celcius"},'kelvin').then(function(result){
+
+    return ratio.converte({value:0,unit:"celcius"},'kelvin').then(function(result){
       expect(result).to.be.deep.equal({value:273.15,unit:'kelvin'});
     });
   });
 
   it('should convert celcius to farenheit',function(){
-    return ratio.converteTemperature({value:33,unit:"celcius"},'farenheit').then(function(result){
+    return ratio.converte({value:33,unit:"celcius"},'farenheit').then(function(result){
       expect(result).to.be.deep.equal({value:91.4,unit:'farenheit'});
     });
   });
 
   it('should throw the correct error if the value is below absolute 0 in kelvin',function(){
-    ratio.converteTemperature({value:-10,unit:"kelvin"},'celcius').catch(function(error){
+    ratio.converte({value:-10,unit:"kelvin"},'celcius').catch(function(error){
       expect(error).to.be.a('Error');
       expect((error).message).to.be.equal('Invalide temperature value');
     });
   });
 
   it('should create a full list of all the supported temperature',function(){
-    return ratio.listTemperature().then((list)=>{
+    return ratio.listTypes().then((list)=>{
+      console.log(list)
       expect(list).to.be.deep.equal([
         { name: 'celcius', displayName: 'Celcius', symbole: '°C' },
         { name: 'farenheit', displayName: 'Farenheit', symbole: '°F' },
